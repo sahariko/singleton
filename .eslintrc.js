04c6c968be77004dbfb9d1e6c20211fc0b3b5f7b
Sahar Brodbeker <saharonb@gmail.com>
2018-11-13 02:23:28 +0200

    Add env module; Switch to jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,19 @@ module.exports = {
   },
   env: {
     node: true,
+    browser: true,
     es6: true
   },
   rules: {
     'no-console': 0,
     'no-var': 2,
     'prefer-const': 2,
-    semi: 2
+    semi: 2,
+    quotes: [2, 'single']
   },
   overrides: [
       {
-          files: ['**/spec.js'],
+          files: ['**/spec.js', '**/*.spec.js'],
           env: {
               mocha: true
           },
