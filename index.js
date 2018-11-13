ef30bbbcfa6c55b4ac31577436c96090c1ea7455
Sahar Brodbeker <saharonb@gmail.com>
2018-11-14 01:30:34 +0200

    Add main class

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-let instance = null;
+import { globalScope } from './lib/env';
+import encode from './lib/encode';
 
-class Singleton {
-  constructor() {
-    if (!instance) {
-      instance = this;
+function Singleton() {
+    let instance;
+    const globalKey = encode(this.constructor.name);
+
+	if (globalScope[globalKey]) {
+        return globalScope[globalKey];
+    } else {
+		instance = this;
+        globalScope[globalKey] = instance;
     }
-
-    console.log('this:', this);
-  }
 }
 
 export default Singleton;
